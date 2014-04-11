edf078ed32a309249c2854656fdc7c4dab41bca5
landrumjc <jlandrum@almondlabs.com>
2014-04-11 16:24:45 -0400

    Updated knockout examples.  Added logfield.js.

---
+++ AlmondLabs.SP13ClientPresentation/Html/js/LogField.js
@@ -0,0 +1,33 @@
+﻿//Register view callbacks
+(function () {
+    function Display(ctx) {
+        console.debug(ctx);
+        return "<div class='AppendOnlyFieldPrefix'></div>";
+    };
+
+    if (typeof SPClientTemplates === 'undefined')
+        return;
+
+    var called = false;
+    var fieldCtx = {};
+    fieldCtx.OnPostRender = function (ctx) {
+        var div = jQuery(".AppendOnlyFieldPrefix").next();
+        if (div.length > 0 && !called) {
+            var html = div.html();
+            var match;
+            while (matches = html.match(/<\/a>\):\s([^<]+)<br>/)) {
+                html = html.replace(matches[1], "<pre style='margin:0px'>" + matches[1] + "</pre>");
+            }
+            div.html(html);
+            called = true;
+        }
+    };
+    fieldCtx.Templates = {};
+    fieldCtx.Templates.Fields = {
+        'Result': {
+            'DisplayForm': Display,
+        }
+    };
+
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(fieldCtx);
+})();
